80ceb6092f5eccbcc3a0d259dc64606e6e4d500e
Alexander <152360979+SOVVET@users.noreply.github.com>
2024-02-09 14:25:39 +0300

    Delete Site directory

--- Site/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container">
-        <div class="tunes">
-        <p>&copy; <?php echo date("Y"); ?> TuNeS - Студия автотюнинга</p>
-    </div>
-</footer>
-
-</body>
-</html>
